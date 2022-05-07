4b6337fb5db139821401330a4ad1f940f6982e7b
Estephane <estephane22@outlook.com>
2022-05-07 16:06:07 -0300

    Repositories Cargo, ContatoEgresso, Contato

---
+++ egresso-api/src/main/java/com/labprog/egresso/model/repositories/ContatoEgressoRepository.java
@@ -0,0 +1,10 @@
+package com.labprog.egresso.model.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContatoEgressoRepository extends JpaRepository<ContatoEgressoRepository, Long>{
+
+}
