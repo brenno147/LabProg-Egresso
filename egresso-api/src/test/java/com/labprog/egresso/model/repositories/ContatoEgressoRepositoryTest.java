4fdcafd0b1712c4a87f5f338ca274157e3f06c36
Estephane <estephane22@outlook.com>
2022-05-11 07:48:12 -0300

    Outros testes ContatoEgresso

--- egresso-api/src/test/java/com/labprog/egresso/model/repositories/ContatoEgressoRepositoryTest.java
+++ egresso-api/src/test/java/com/labprog/egresso/model/repositories/ContatoEgressoRepositoryTest.java
@@ -34,6 +34,7 @@ public class ContatoEgressoRepositoryTest {
     @Test
     public void deveSalvarContatoEgresso(){
 
+        // Cenário: Criar ContatoEgresso
         Egresso egressoSalvo = egressoRepository.save(Egresso.builder().nome("Abc").email("a@a").cpf("1234567").build());
         Contato contatoSalvo = contatoRepository.save(Contato.builder().nome("Celular").url_logo("1234567").build());
 
@@ -46,8 +47,91 @@ public class ContatoEgressoRepositoryTest {
                 .egresso(egressoSalvo)
                 .build();
 
+        // Ação: Salvar ContatoEgresso
         ContatoEgresso salvo = contatoEgressoRepository.save(contato);
 
+        // Verificação: Se o contato foi salvo
         Assertions.assertNotNull(salvo);
+    }
+
+    @Test
+    public void deveRetornarTodosAsOcorrenciasDaTabelaContatoEgresso(){
+
+        // Cenário: Criar ContatoEgresso
+        Egresso egressoSalvo = egressoRepository.save(Egresso.builder().nome("Abc").email("a@a").cpf("1234567").build());
+        Contato contatoSalvo = contatoRepository.save(Contato.builder().nome("Celular").url_logo("1234567").build());
+
+        ContatoEgresso contato = ContatoEgresso.builder()
+                .id(ContatoEgressoPK.builder()
+                        .contato_id(contatoSalvo.getId())
+                        .egresso_id(egressoSalvo.getIdEgresso())
+                        .build())
+                .contato(contatoSalvo)
+                .egresso(egressoSalvo)
+                .build();
+        
+        // Ação: Salvar ContatoEgresso e encontrar todos as ocorências
+
+        ContatoEgresso salvo = contatoEgressoRepository.save(contato);
+        List<ContatoEgresso> contatosEgresso = contatoEgressoRepository.findAll();
+
+        // Verificação: Se a lista é maior que 0 
+        Assertions.assertNotNull(salvo);
+        Assertions.assertTrue(contatosEgresso.size() > 0);
+    }
+
+    @Test
+    public void deveRetornarAQuantidadeDeLinhasPresentesEmContatoEgresso(){
+
+        // Cenário: Criar ContatoEgresso
+        Egresso egressoSalvo = egressoRepository.save(Egresso.builder().nome("Abc").email("a@a").cpf("1234567").build());
+        Contato contatoSalvo = contatoRepository.save(Contato.builder().nome("Celular").url_logo("1234567").build());
+
+        ContatoEgresso contato = ContatoEgresso.builder()
+                .id(ContatoEgressoPK.builder()
+                        .contato_id(contatoSalvo.getId())
+                        .egresso_id(egressoSalvo.getIdEgresso())
+                        .build())
+                .contato(contatoSalvo)
+                .egresso(egressoSalvo)
+                .build();
+        
+        // Ação: Obter a quantidade de ContatoEgresso na tabela
+
+        ContatoEgresso salvo = contatoEgressoRepository.save(contato);
+        Long count  = contatoEgressoRepository.count();
+
+        // Verificação: Se a quantidade retornada é maior que 0
+        Assertions.assertNotNull(salvo);
+        Assertions.assertTrue(count > 0);
+    }
+
+    @Test
+    public void deveDeletarTodasAsOcorrenciasDeContatoEgresso(){
+
+        // Cenário: Criar ContatoEgresso
+
+        Egresso egressoSalvo = egressoRepository.save(Egresso.builder().nome("Abc").email("a@a").cpf("1234567").build());
+        Contato contatoSalvo = contatoRepository.save(Contato.builder().nome("Celular").url_logo("1234567").build());
+
+        ContatoEgresso contato = ContatoEgresso.builder()
+                .id(ContatoEgressoPK.builder()
+                        .contato_id(contatoSalvo.getId())
+                        .egresso_id(egressoSalvo.getIdEgresso())
+                        .build())
+                .contato(contatoSalvo)
+                .egresso(egressoSalvo)
+                .build();
+        
+        // Ação: Deletar todos os contatos da tabela
+
+        ContatoEgresso salvo = contatoEgressoRepository.save(contato);
+        contatoEgressoRepository.deleteAll();
+
+        Long count = contatoEgressoRepository.count();
+
+        // Verificação: Se não existem ContatoEgresso nessa tabela
+        Assertions.assertNotNull(salvo);
+        Assertions.assertTrue(count==0);
     }
 }
