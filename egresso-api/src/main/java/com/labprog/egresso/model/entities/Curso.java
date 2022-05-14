419fdeccc3da5f2a353860f841ee75e2bf1dc7e1
Brenno Izaias <brenno_izaias@hotmail.com>
2022-05-14 08:57:15 -0300

    Alteracaoes entidades de relacionamento

--- egresso-api/src/main/java/com/labprog/egresso/model/entities/Curso.java
+++ egresso-api/src/main/java/com/labprog/egresso/model/entities/Curso.java
@@ -1,14 +1,15 @@
 package com.labprog.egresso.model.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
@@ -25,5 +26,8 @@ public class Curso {
   private String nome;
 
   private String nivel;
+
+  @OneToMany(mappedBy = "curso")
+  private List<CursoEgresso> datas_inicio_fim_egressos = new ArrayList<>();
 
 }
