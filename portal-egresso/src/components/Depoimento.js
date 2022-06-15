353b8f7dc115fcec8ab959d052822347e1fc4648
brenno.nascimento <brenno.nascimento@tce.ma.gov.br>
2022-06-15 11:19:35 -0300

    Criação componente Depoimento

--- portal-egresso/src/components/Depoimento.js
+++ portal-egresso/src/components/Depoimento.js
@@ -1,26 +1,45 @@
 import React from "react";
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import logo from "./../imgs/thumbnail.svg";
 
 const Depoimento = () => {
+
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    };
+
     return ( 
-    <div class="accordion-item">
-        <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                <div class="row">
-                    <div class="col-md-2">
-                        <img src="https://via.placeholder.com/150" alt=""/>
+        <div class="m-4">
+            <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1bh-content"
+                    id="panel1bh-header"
+                >
+                    <div className="d-flex flex-row">
+                        <div className="mr-3">
+                            <img src={logo} alt="avatar" style={{height: "100px"}} />
+                        </div>
+                        <div className="d-flex flex-column justify-content-center">
+                            <h5 className="text-center">Depoimento Sara Archidi</h5>
+                        </div>
                     </div>
-                    <div class="col-md-10">
-                        <strong>This is the first item's accordion body.</strong> It is shown by default, until the...
-                    </div>
-                </div>
-            </button>
-        </h2>
-        <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
-            <div class="accordion-body">
-                <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-            </div>
+
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
+                        Aliquam eget maximus est, id dignissim quam.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
         </div>
-    </div> 
     );
 }
  
