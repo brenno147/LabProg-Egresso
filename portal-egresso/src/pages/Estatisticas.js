46864b2cf76d0ed7d296c8e58bcb9f09198ba4d3
felipehads <felipehads@outlook.com>
2022-07-19 11:36:42 -0300

    Gráficos de números de egresso por faixa salário, curso e cargo, apenas para valores maior que 0

--- portal-egresso/src/pages/Estatisticas.js
+++ portal-egresso/src/pages/Estatisticas.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Charts from "../components/Charts";
 import Footer from "../components/Footer";
 import NavbarComponent from "../components/NavbarComponent";
@@ -8,13 +8,43 @@ import cursoService from "../services/CursoService";
 import cargoService from "../services/CargoService";
 
 function Estatisticas(){
-    const fsService= new faixaSalarioService()
+    const fsService = new faixaSalarioService()
     const crsService = new cursoService()
     const crgService = new cargoService()
 
-    console.log(fsService.quantEgressosPorFaixaSalario())
-    console.log(crsService.quantEgressosPorCurso());
-    console.log(crgService.quantEgressosPorCargo());
+    let [faixaSalarioData, setFaixaSalarioData] = useState([])
+    let [cursoData, setCursoData] = useState([])
+    let [cargoData, setCargoData] = useState([])
+    let [fsArrayLabels, setFsArrayLabels] = useState();
+    let [crsArrayLabels, setCrsArrayLabels] = useState();
+    let [crgArrayLabels, setCrgArrayLabels] = useState();
+    let fsDataAux, crsDataAux, crgDataAux
+    
+
+    useEffect( () => {
+      async function fectchData() {
+        fsDataAux = await fsService.quantEgressosPorFaixaSalario();
+        crsDataAux = await crsService.quantEgressosPorCurso()
+        crgDataAux = await crgService.quantEgressosPorCargo()
+
+
+
+        setFsArrayLabels(fsDataAux.data.map((item) => item.faixaSalario))
+        setCrsArrayLabels(crsDataAux.data.map((item) => item.curso));
+        setCrgArrayLabels(crgDataAux.data.map((item) => item.cargo));
+
+
+        setFaixaSalarioData(fsDataAux.data.map((item) => item.numEgresso))
+        setCursoData(crsDataAux.data.map((item) => item.numEgresso));
+        setCargoData(crgDataAux.data.map((item) => item.numEgresso));
+
+        //ADICIONANDO DADOS ESTÁTICOS
+        setFsArrayLabels((prevState) => [...prevState, 3, 4])
+        setCrgArrayLabels((prevState) => [...prevState, 3, 4, 5, 6]);
+    
+      }
+      fectchData()
+    }, [])
 
     return (
       <div>
@@ -24,11 +54,26 @@ function Estatisticas(){
           style={{ backgroundColor: "rgba(189, 205, 236, 1)" }}
         >
           <p className="h1 mb-4">Estatísticas</p>
-          <Charts description="Egressos por Faixa Salarial" />
+          <Charts 
+            chartData={faixaSalarioData} 
+            labels={fsArrayLabels} 
+            description="Egressos por Faixa Salarial"
+            text="Faixas Salariais"
+            />
 
-          <Charts description="Egressos por Curso" />
+          <Charts 
+            chartData={cursoData} 
+            labels={crsArrayLabels} 
+            description="Egressos por Curso" 
+            text="Cursos"
+            />
 
-          <Charts description="Egressos por Cargo" />
+          <Charts 
+            chartData={cargoData} 
+            labels={crgArrayLabels} 
+            description="Egressos por Cargo"
+            text="Cargos"
+          />
         </div>
         <Footer />
       </div>
